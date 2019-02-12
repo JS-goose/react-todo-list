594ef94bed9d0e0ff33787133ff469d2f9f230c8
Jonathan Sexton <joncsexton@gmail.com>
2019-02-12 16:30:32 -0600

    testing the display method on todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Todos extends Component {
   render() {
     return this.props.todos.map((todo) => (
-        
+         <div>Name: {todo.title}</div>
     ))
   }
 }
