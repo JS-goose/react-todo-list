337c33fef3b9d738c42b94090a456fca94c88727
Jonathan Sexton <joncsexton@gmail.com>
2019-02-12 05:32:05 -0600

    built todos component and basic structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Here's an H1</h1>
+      <h1>Here's the title</h1>
+      <Todos />
       </div>
     );
   }
---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Here's the todo component</h1>
+      </div>
+    );
+  }
+}
+
+export default Todos;
