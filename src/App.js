fcc764a050163e087f597bfdebbb4543d0e79e3c
Jonathan Sexton <joncsexton@gmail.com>
2019-02-12 05:34:11 -0600

    added state with todos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Buy Wand',
+        complete: false
+      },
+      {
+        id: 2,
+        title: 'Buy Broom',
+        complete: false
+      },
+      {
+        id: 3,
+        title: 'Defeat Voldemort',
+        complete: false
+      },
+    ]
+  }
   render() {
     return (
       <div className="App">
