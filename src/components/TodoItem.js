4d6f165243e477d979ab764d02bf7ddcd45e042d
Jonathan Sexton <joncsexton@gmail.com>
2019-02-12 17:26:45 -0600

    moving makeComplete function to app.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,19 +4,21 @@ import PropTypes from "prop-types";
 export class TodoItem extends Component {
   getStyle = () => {
     return {
-     backgroundColor: '#f4f4f4',
-     padding: '10px',
-     borderBottom: '1px solid black',
-     color: this.props.todo.complete ? 'red' : 'black',
-     textDecoration: this.props.todo.complete ? 'line-through' : "none",
-    }
+      backgroundColor: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px solid black",
+      color: this.props.todo.complete ? "red" : "black",
+      textDecoration: this.props.todo.complete ? "line-through" : "none",
+    };
   };
 
   render() {
     return (
-      <div>
-        <input type="checkbox" onChange = {this.makeComplete)/>
-        <p style={this.getStyle()}>Name: {this.props.todo.title}</p>
+      <div style={this.getStyle()}>
+        <p>
+          <input type="checkbox" onChange={this.makeComplete} /> {' '}
+          Name: {this.props.todo.title}
+        </p>
       </div>
     );
   }
