2e8f6da4d0a3fde1eabe1a1a423f4b2fe78fe010
Jonathan Sexton <joncsexton@gmail.com>
2019-02-13 04:35:50 -0600

    updated title

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Here's the title</h1>
+        <h1>React Todo List</h1>
         <Todos todos={this.state.todos} makeComplete={this.makeComplete} />
       </div>
     );
