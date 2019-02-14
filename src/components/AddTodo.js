fa10d744937533ffafa46e20c2f47c288e02af7d
Jonathan Sexton <joncsexton@gmail.com>
2019-02-13 19:50:38 -0600

    adding state to AddTodo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 
 export class AddTodo extends Component {
+  state = {
+    title: "",
+  };
+
   render() {
     return (
-      <form style={{ display: 'flex' }}>
-        <input type="text" name="title" placeholder="Add todo item..." style={{flex: '10', padding: '10px'}}/>
-        <input type="submit" value="Submit" className="addBtn" style={{flex: '1'}}/>
+      <form style={{ display: "flex" }}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Add todo item..."
+          value={this.state.title}
+          style={{ flex: "10", padding: "10px" }}
+        />
+        <input type="submit" value="Submit" className="addBtn" style={{ flex: "1" }} />
       </form>
     );
   }
