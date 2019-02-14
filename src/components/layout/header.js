97c79e3f7a47ae73b00e82f3da43373ca450a105
Jonathan Sexton <joncsexton@.com>
2019-02-14 15:01:34 -0600

    formatting change & env file

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1 style = {titleStyle}>React Todo List</h1>
+      <h1 style={titleStyle}>React Todo List</h1>
     </header>
   );
 }
@@ -12,10 +12,12 @@ const titleStyle = {
   textAlign: "center",
   padding: "5px 0",
   borderBottom: "1px solid gray",
-  borderTopLeftRadius: '5px',
-  borderTopRightRadius: '5px',
+  borderTopLeftRadius: "5px",
+  borderTopRightRadius: "5px",
   color: "#0180ff",
-  backgroundColor: '#333',
+  backgroundColor: "#333",
 };
+
+
 
 export default Header;
