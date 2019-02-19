f2abe374b56b06c56a4c75b051f6e404ff1e9efe
Jonathan Sexton <joncsexton@gmail.com>
2019-02-19 04:27:34 -0600

    added content and basic styling on About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React from "react";
 
 export default function About() {
-    return (
-        <React.Fragment>
-            <h1>About</h1>
-            <p>This is the about page</p>
-        </React.Fragment>
-    )
+  return (
+    <div style={containerStyle}>
+      <h1>About</h1>
+      <p>
+        This project was built with nothing but React, React Router, and Axios. The todos in the
+        list are brought in via{" "}
+        <a href="https://jsonplaceholder.typicode.com/" target="_blank" rel="noopener noreferrer">
+          JSONPlaceholder
+        </a>
+        .
+      </p>
+      <hr />
+      <p>
+        If you wish to use this project yourself you'll need to fork or download the repository.
+        Once on your local machine you'll need to run <code>npm install</code> in the project
+        directory. If you wish to use this as a production build you'll need to run{" "}
+        <code>npm run build</code> in the project directory on your local machine.
+      </p>
+    </div>
+  );
+}
+
+const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '10px',
+    lineHeight: '1.6',
 }
